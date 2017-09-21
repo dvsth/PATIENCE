c78b15466b8aa47db485e35e62e9db3f75b70edc
dvsth <dfirespark@gmail.com>
2017-09-21 18:07:33 +0530

    started boris

--- mag_code/boris_constant_magnetic_field_only.py
+++ mag_code/boris_constant_magnetic_field_only.py
@@ -1,3 +1,22 @@
+#
+# Have you heard the tragedy of Frog Leaper the Wise? It's not a story 
+# a programmer would tell you. Once upon a time, in the days of yore,
+# when PATIENCE was still in development, many brave souls made their 
+# efforts to simulate the dangerous and elusive magnetic field. Euler
+# was the bold venturer, ever ready to fight in the name of science. 
+# He drew first blood from the simulator, but alas, he could only 
+# simulate electric fields with his inferior first-order method. 
+# Runge-Kutta came next, taking the baton from 
+# Euler as champions of accuracy, pioneering with their second-order
+# scheme. It was not enough. Weep, O, you programmers, for under
+# Runge-Kutta it was that the dark side started to gain victory.
+# Blood was spilled by the megabytes in the form of energy leakage,
+# for though our champions were accurate, their valiant scheme 
+# failed to conserve the energy that was the lifeblood of the magnetic
+# field. Particles spiraled out of control, struggling to maintain a 
+# circular path but eventually fading away into the depths of magnitude
+# where even the most brave dare not venture.
+
 import numpy as np
 import matplotlib.pyplot as plt
 import ConfigParser
@@ -28,10 +47,10 @@ h = t_final/steps
 plot_pos_x = list()
 plot_pos_y = list()
 
-#leapfrog variables
-vel_plus_half = [ 0. , 0. , 0.]
-vel_minus_half = [ 0. , 0. , 0.]
-
+#boris variables
+a_0 = np.multiply( charge/mass , np.cross( vel_0 , B ) )
+t_vector = np.multiply(charge * h / mass * 0.5 , B)
+s_vector = np.divide( np.multiply(2. , t_vector) , 1 + (np.linalg.norm(t_vector)**2))
 #--------------------- run the simulation
 
 #calculate the magnetic field as a function of time
@@ -43,7 +62,7 @@ def acceleration ( BField , velocity ) :
 	return np.multiply(charge/mass , np.cross( velocity , BField ))
 	
 #calculate the velocity	
-def velocityUpdate ( h , v_prev , t ) :
+def velocityUpdate ( h , x_prev , v_prev , t ) :
 	return np.add(v_prev ,  np.multiply( h , acceleration( field(t) , v_prev )))
 	
 #calculate the position
@@ -51,48 +70,21 @@ def positionUpdate ( h , x_prev , v_prev , v_new , t ) :
 	return np.add(x_prev , np.multiply(h/2.0 , np.add(v_new , v_prev)))
 	
 #algorithm
-
-print ""
-print "###########################################"
-print "#         commence leapfrogging ...       #"
-print "###########################################"
-print "#                                         #"               
-print "#               _  --  _                  #"
-print "#            -            -               #"
-print "#         -                  -            #"
-print "#       -                      -          #"
-print "#      -                        -         #"
-print "#     _                          _        #"
-print "#                                         #"
-print "#          wow, what a leap               #"
-print "#                                         #"
-print "#  this is one small step for a compiler, #"
-print "#     but a giant leap for PATIENCE.      #"
-print "#                                         #"  
-print "###########################################"
-print ""
-
-#compute half-step leapfrog starter
-a_0 = np.multiply( charge/mass , np.cross( vel_0 , B ) )
-vel_minus_half = np.subtract( vel_0 , np.multiply( h/2. , a_0 ))
-
-#begin integration
-for step in range(steps):
-	time = step*h
+for step in range(steps) :
 	
-	vel_plus_half = velocityUpdate( h , vel_minus_half , time )
-	pos_1 = positionUpdate( h , pos_0 , vel_minus_half , vel_plus_half , time )
-	
+	v_minus = np.add(vel_0 , np.multiply(h/2. , a_0))
+	v_prime = np.add(v_minus , np.cross(v_minus , t_vector))
+	v_plus = np.subtract( v_minus , np.cross(v_prime , s_vector))
 	plot_pos_x.append(pos_1[0])
 	plot_pos_y.append(pos_1[1])
 	
-	vel_minus_half = vel_plus_half
+	vel_0 = vel_1
 	pos_0 = pos_1
 	
 #--------------------- generate the plot
 fig = plt.figure()
 
-fig.suptitle('Trajectory for a proton \n computed using \n Leapfrog scheme \n in ' + str(steps) + " steps" , fontweight='bold')
+fig.suptitle('Trajectory for a proton \n computed using \n Runge-Kutta 2 scheme \n in ' + str(steps) + " steps" , fontweight='bold')
 pltPos = fig.add_subplot(111)
 
 pltPos.grid(b=True, color='k', linestyle='--')
@@ -106,8 +98,6 @@ pltPos.scatter(plot_pos_x[-1] , plot_pos_y[-1], color='k' , s=100,marker='x' , l
 pltPos.set_aspect(1.0)
 plt.legend(loc='best')
 plt.show()
-
-np.savetxt('leapfrog.csv')
 
 
 
